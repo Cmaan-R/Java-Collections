9be13501d62065d0f23b556028828045a5a56d6f
Windows 10 <Windows 10@192.168.200.171>
2022-10-05 16:30:32 -0300

    Update

--- gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
@@ -6,19 +6,30 @@ import java.util.Set;
 public class TestaAlunos {
 
 	public static void main(String[] args) {
-		
+
 		Set<String> alunos = new HashSet<>();
 		alunos.add("Rodrigo Turini");
+		alunos.add("Alberto Souza");
 		alunos.add("Alberto Souza");
 		alunos.add("Nico Steppad");
 		alunos.add("Sergio Lopes");
 		alunos.add("Renan Saggio");
 		alunos.add("Mauricio Aniche");
+
+		boolean pauloEstaMatriculado = alunos.contains("Paulo Silveira");
+		System.out.println(pauloEstaMatriculado);
 		
-		
+		alunos.remove("Sergio Lopes");
+
+		System.out.println(alunos.size());
+
 		for (String aluno : alunos) {
 			System.out.println(aluno);
 		}
+
+		alunos.forEach(aluno -> {
+			System.out.println(aluno);
+		});
 
 		System.out.println(alunos);
 	}
